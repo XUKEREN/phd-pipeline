f7c8e7beeac8937b723ce3ff9383cec3f8744d80
XUKEREN <xukeren418@gmail.com>
2023-06-25 16:29:34 -0700

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,9 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..')) # or "../../src
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,7 +16,12 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",  # automatically generate documentation for modules
+    "sphinx.ext.napoleon",  # to read Google-style or Numpy-style docstrings
+    "sphinx.ext.viewcode",  # to allow vieing the source code in the web page
+    "autodocsumm",  # to generate tables of functions, attributes, methods, etc.
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,3 +33,7 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+# don't include docstrings from the parent class
+autodoc_inherit_docstrings = False
+# Show types only in descriptions, not in signatures
+autodoc_typehints = "description"
