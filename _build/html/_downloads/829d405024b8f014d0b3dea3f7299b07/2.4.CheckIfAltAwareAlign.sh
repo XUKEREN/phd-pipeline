481099c8656b74b43ab86c924d23f7830b07e20a
Keren Xu <keren.xu@tempus.com>
2023-07-28 10:36:26 -0700

    add git action

--- _build/html/_downloads/829d405024b8f014d0b3dea3f7299b07/2.4.CheckIfAltAwareAlign.sh
+++
@@ -1,4 +0,0 @@
-# check if alt-aware alignment
-# from this post
-# https://gatk.broadinstitute.org/hc/en-us/articles/360037498992--How-to-Map-reads-to-a-reference-with-alternate-contigs-like-GRCH38
-samtools view -H *.bam
